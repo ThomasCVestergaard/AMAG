d81079c87e15d1c70c7cbe4330fbbe9a1e819c9e
Thomas Vestergaard <thomascvestergaard@gmail.com>
2017-10-13 14:21:24 +0200

    Python script copied into Grasshopper Python in order to access geometry more easily. Random vector created, but only with 1 vector at a time

---
+++ RayTracing_01.py
@@ -0,0 +1,32 @@
+﻿import random
+import math
+import rhinoscriptsyntax as rs
+    
+"""Ray Vector Points"""
+def RayPoints(theta,phi):
+    point=rs.AddPoint(math.sin(theta)*math.cos(phi), math.sin(theta)*math.sin(phi), math.cos(theta))
+    return point
+    
+"""Ray Vector Origo Points"""
+def OrigoPoints(x,y,z):
+    point=rs.AddPoint(x,y,z)
+    return point
+    
+"""Ray Vectors between Ray Vector Points and Origo"""
+def RayVectors(RayPoints,OrigoPoints):
+    Vector=rs.VectorCreate(RayPoints,OrigoPoints)
+    return Vector
+    
+RayVec=[]
+for i in range (n):
+    
+    theta = random.uniform(0,(2*math.pi))  
+    phi = random.uniform(0,(2*math.pi))
+    
+    x = 0
+    y = 0
+    z = 0
+    
+RayVec.append(RayVectors(RayPoints(theta,phi),OrigoPoints(x,y,z)))
+
+print RayVec
