1d7d0a6542b0ba46ff164869688bd370ac27538f
Thomas Vestergaard <thomascvestergaard@gmail.com>
2017-11-03 09:55:13 +0100

    Normal Vectors for geometry defined

--- RayTracing.py
+++ RayTracing.py
@@ -3,28 +3,30 @@ import math
 import rhinoscriptsyntax as rs
 import Rhino as rc
     
-    
-"""Ray Vector Points"""
+Geo=rc.Geometry.Brep.Duplicate(G)
+
+#Ray Vector Points
 def RayPoints(theta,phi):
     point=rs.AddPoint(math.sin(theta)*math.cos(phi), math.sin(theta)*math.sin(phi), math.cos(theta))
     return point
     
     
-"""Ray Vector Origo Points"""
+#Ray Vector Origo Points
 def OrigoPoints(x,y,z):
     point=rs.AddPoint(x,y,z)
     return point
     
     
-"""Ray Vectors between Ray Vector Points and Origo"""
+#Ray Vectors between Ray Vector Points and Origo
 def RayVectors(RayPoints,OrigoPoints):
     Vector=rs.VectorCreate(RayPoints,OrigoPoints)
     return Vector
     
     
-"""RayVectors"""
+#RayVectors
 RayVec=[]
-#print range(int(n))
+
+print range(int(n))
 for i in range (n):
     
     theta = random.uniform(0,(2*math.pi))  
@@ -36,30 +38,33 @@ for i in range (n):
     
     RayVec.append(RayVectors(RayPoints(theta,phi),OrigoPoints(x,y,z)))
 
-#print RayVec
+print RayVec
 
 
-"""Testing weither source point is contained within Geometry"""
-CS = G.IsPointInside(SP,0.01,True)
+#Testing weither source point is contained within Geometry
+CS = Geo.IsPointInside(SP,0.01,True)
 
-"""Number of Surfaces in Geometry"""
-F=G.Surfaces.Count
+#Number of Surfaces in Geometry
+F=Geo.Surfaces.Count
 
-"""Extracting Surfaces from Geometry"""
-Faces=[]
-def faces(f):
-    faces=G.Faces.ExtractFace(f)
-    return faces
+#Extracting Surfaces from Geometry
+
+Surfaces=[]
+def Surf(f):
+    surface=rs.ExtractSurface(G,f,copy=True)
+    return surface[0]
     
+print range (int(F))
 for f in range (F):
     
-    Faces.append(faces(f))
-    
-print Faces
+    Surfaces.append(Surf(f))
 
+print Surfaces
+
+#Extracting Surface Points from Geometry Surfaces
 SurfPoints=[]
 def SurfacePoints(f):
-    point=rs.SurfaceAreaCentroid(Faces[f])
+    point=rs.SurfaceAreaCentroid(Surfaces[f])
     return point[0]
 
 print range (int(F))
@@ -67,4 +72,29 @@ for f in range (F):
     
     SurfPoints.append(SurfacePoints(f))
     
+
 print SurfPoints
+
+#Surface Normal Vectors
+
+Param=[]
+def UV_Param(f):
+    point=rs.SurfaceClosestPoint(Surfaces[f],SurfPoints[f])
+    return point
+    
+print range (int(F))
+for f in range (F):
+    
+    Param.append(UV_Param(f))
+    
+SurfNormal=[]
+def SurfaceNormal(f):
+    vector=rs.VectorReverse(rs.SurfaceNormal(Surfaces[f],param))
+    return vector
+    
+print range (int(F))
+for f in range (F):
+    
+    SurfNormal.append(SurfaceNormal(f))
+    
+print SurfNormal
