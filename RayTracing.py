2ba1cccc4ca8793781fbe20a6808409ba52d56c5
Thomas Vestergaard <thomascvestergaard@gmail.com>
2017-11-17 16:30:07 +0100

    Bug with rays escaping geometry fixed

--- RayTracing.py
+++ RayTracing.py
@@ -10,104 +10,114 @@ Origo=rs.AddPoint(0,0,0)
 CS = Geo.IsPointInside(SP,0.01,True)
 
 #Number of Surfaces in Geometry
-F=Geo.Surfaces.Count
+F = Geo.Surfaces.Count
 
 #Extracting Surfaces from Geometry
-Surfaces=[]
+Surfaces = []
 def Surf(f):
-    surface=rs.ExtractSurface(G,f,copy=True)
+    surface = rs.ExtractSurface(G,f,copy=True)
     return surface[0]
 print range (int(F))
 for f in range (F):
     Surfaces.append(Surf(f))
 
 #Extracting Surface Points from Geometry Surfaces
-SurfPoints=[]
+SurfPoints = []
 def SurfacePoints(f):
-    point=rs.SurfaceAreaCentroid(Surfaces[f])
+    point = rs.SurfaceAreaCentroid(Surfaces[f])
     return point[0]
 print range (int(F))
 for f in range (F):
     SurfPoints.append(SurfacePoints(f))
 
 #Surface Normal Vectors
-Param=[]
+Param = []
 def UV_Param(f):
-    point=rs.SurfaceClosestPoint(Surfaces[f],SurfPoints[f])
+    point = rs.SurfaceClosestPoint(Surfaces[f],SurfPoints[f])
     return point[0]
 print range (int(F))
 for f in range (F):
     Param.append(UV_Param(f))
     
-SurfNormal=[]
+SurfNormal = []
 def SurfaceNormal(f):
-    vector=rs.VectorReverse(rs.SurfaceNormal(Surfaces[f],Param))
+    vector = rs.VectorReverse(rs.SurfaceNormal(Surfaces[f],Param))
     return vector
-print range (int(F))
 for f in range (F):
     SurfNormal.append(SurfaceNormal(f))
     
-
+    
 #Ray Vector Points
 def RayPoints(theta,phi):
-    point=rs.AddPoint(math.sin(theta)*math.cos(phi), math.sin(theta)*math.sin(phi), math.cos(theta))
+    point = rs.AddPoint(math.sin(theta)*math.cos(phi),math.sin(theta)*math.sin(phi),math.cos(theta))
     return point
 #Ray Vectors between Ray Vector Points and Origo
 def RayVectors(RayPoints,OrigoPoints):
-    Vector=rs.VectorCreate(RayPoints,Origo)
+    Vector = rs.VectorCreate(RayPoints,Origo)
     return Vector
     
 #RayVectors
-RayVec=[]
+RayVec = []
 print range(int(n))
 for i in range (n):
     theta = random.uniform(0,(2*math.pi))  
     phi = random.uniform(0,(2*math.pi))
     RayVec.append(RayVectors(RayPoints(theta,phi),Origo))
 
-
-ReflectionPoints=[SP]
-Rays=[]
-RayData=[]
+ReflectionPoints = [SP]
+RefVec = [RayVec[0]]
+Distances = []
+MinDistances = []
+Rays = []
+RayData = []
 RayData.append(SP)
 RayData.append(RayVec[0])
 
 r = R
 while r > 0:
-
-    #Vector From Source to Geometry planes
-    LenVec=[]
+    #Vector From Source to Geometry Surfaces
+    LenVec = []
     def LengthVectors(f):
-        vector=rs.VectorCreate(SurfPoints[f],RayData[0])
+        vector = rs.VectorCreate(SurfPoints[f],RayData[0])
         return vector
-    print range (int(F))
     for f in range (F):
         LenVec.append(LengthVectors(f))
-
+    
+    Dist = []
     #Distance from Source Point to Geometry Surfaces
-    Dist=[]
     def Distance(f):
-        length=rs.VectorDotProduct(SurfNormal[f],LenVec[f])/rs.VectorDotProduct(RayData[1],SurfNormal[f])
+        length = (rs.VectorDotProduct(SurfNormal[f],LenVec[f]))/(rs.VectorDotProduct(RayData[1],SurfNormal[f]))
         return length
-    print range (int(F))
     for f in range (F):
         Dist.append(Distance(f))
     
+        Distances.append(Distance(f))
+    
     #Minimum distance
-    DistMin=min([n for n in Dist  if n>0])
-    index=[i for i,x in enumerate(Dist) if x == DistMin]
-
+    DistMin = min(n for n in Dist if n > 1*10**-12)
+    index = [i for i,x in enumerate(Dist) if x == DistMin]
+    
+    MinDistances.append(DistMin)
+    MinDistances.append(index[0])
     #Reflection Point
-    ReflectionPoint=RayData[0]+RayData[1]*DistMin
+    ReflectionPoint = RayData[0]+RayData[1]*DistMin
     
     ReflectionPoints.append(ReflectionPoint)
     ReflectPoints=ReflectionPoints[:-1]
     
     Rays.append(rs.VectorCreate(ReflectionPoint,RayData[0]))
-
+    
     #Reflected Vector
-    ReflectionVec=RayData[1]-2*(rs.VectorDotProduct(SurfNormal[index[0]],RayData[1]))*SurfNormal[index[0]]
+    ReflectionVec = rs.VectorSubtract(RayData[1],(2*SurfNormal[index[0]]*rs.VectorDotProduct(SurfNormal[index[0]],RayData[1])))
+    NormVec=rs.VectorCrossProduct(RayData[1],ReflectionVec)
+    #if  rs.VectorAngle(SurfNormal[index[0]],ReflectionVec) > 90:
+        #ReflectionVec=rs.VectorRotate(ReflectionVec,180-2*rs.VectorAngle(SurfNormal[index[0]],ReflectionVec),NormVec)
+    
+    print ReflectionVec
+    RefVec.append(ReflectionVec)
+    RefVector = RefVec[:-1]
     
     RayData=(ReflectionPoint,ReflectionVec)
     
     r = r-1
+
